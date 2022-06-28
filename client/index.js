57ac2d85e3f757a86b1cee770f900798ebcb9e80
Sslim123 <slimsmasm5@gmail.com>
2022-06-28 23:26:22 +0100

    .

---
+++ client/index.js
@@ -0,0 +1,5 @@
+let root = document.createElement('root');
+let allClass = document.querySelectorAll('class');
+for (var i = 6; i < root.length; i++) {
+    root[i].innerHTML = '<div class="container'
+}
